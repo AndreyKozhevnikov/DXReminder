c73d89bdd9889660966c811571085c790287499b
kozhevnikov.andrey <kozhevnikov.andrey@KOZHEVNIKOV-W10>
2016-07-07 17:12:14 +0300

    serializer create XML

--- DXReminder/Classes/ReminderStore.cs
+++ DXReminder/Classes/ReminderStore.cs
@@ -6,15 +6,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Xml.Linq;
 
 namespace DXReminder.Classes {
-    public class ReminderStore {
+    public class Reminder {
         
         public int DayOfWeek { get; set; }
         public DateTime Time { get; set; }
         public string Description { get; set; }
 
-        public ReminderStore(string _description, int _dayOfWeek, DateTime _time) {
+        public Reminder(string _description, int _dayOfWeek, DateTime _time) {
             Description = _description;
             DayOfWeek = _dayOfWeek;
             Time = _time;
@@ -24,16 +25,27 @@ namespace DXReminder.Classes {
             string st = string.Format("{0} - {1} - {2}", Description, s, Time.ToString("HH:mm"));
             return st;
         }
+
+        internal XElement GetXML() {
+            XElement xl = new XElement("Reminder");
+            xl.Add(new XAttribute("Description", Description));
+            xl.Add(new XAttribute("Time", Time.ToString("HH:mm")));
+            xl.Add(new XAttribute("DayOfWeek", DayOfWeek));
+            return xl;
+        }
+
+
     }
 
     public class BaseViewModel {
         public BaseViewModel() {
-            Reminders = new ObservableCollection<ReminderStore>();
+            Reminders = new ObservableCollection<Reminder>();
+            Temp_CreateReminder();
         
         }
-        public ObservableCollection<ReminderStore> Reminders { get; set; }
+        public ObservableCollection<Reminder> Reminders { get; set; }
         ICommand _addNewReminderCommand;
-
+        ReminderSerializer serializer;
         public string TempDescription { get; set; }
         public int TempDayOfWeek { get; set; }
         public DateTime TempTime { get; set; }
@@ -47,10 +59,61 @@ namespace DXReminder.Classes {
         }
 
         private void AddNewReminder() {
-            ReminderStore r = new ReminderStore(TempDescription, TempDayOfWeek, TempTime);
+            Reminder r = new Reminder(TempDescription, TempDayOfWeek, TempTime);
             Reminders.Add(r);
         }
+
+        void Temp_CreateReminder() {
+            Reminders.Add(new Reminder("test", 1, new DateTime(1, 1, 1, 23, 23, 0)));
+            Reminders.Add(new Reminder("test2", 1, new DateTime(1, 1, 1, 11, 24, 0)));
+        }
+
+        void Serialize() {
+            CreateSerializeIfNeeded();
+            serializer.Serialize(Reminders);
+        }
+
+        private void CreateSerializeIfNeeded() {
+            if (serializer == null)
+                serializer = new ReminderSerializer();
+        }
+
+
+
+
+
+        #region For_Test
+        public void Test_CreateSerializerIfNeeded() {
+            CreateSerializeIfNeeded();
+        }
+        public ReminderSerializer Test_Serializer {
+            get { return serializer; }
+        }
+
+        #endregion
     }
+
+    public class ReminderSerializer {
+
+        internal void Serialize(ObservableCollection<Reminder> reminders) {
+            XElement st = GetXMLFromReminders(reminders);
+        }
+
+        private XElement GetXMLFromReminders(ObservableCollection<Reminder> reminders) {
+            XElement xlBase = new XElement("Reminders");
+            foreach (Reminder r in reminders) {
+                XElement x = r.GetXML();
+                xlBase.Add(x);
+            }
+            return xlBase;
+        }
+
+        public XElement Test_GetXMLFromReminders(ObservableCollection <Reminder> reminders) {
+            return GetXMLFromReminders(reminders);
+        }
+
+    }
+
 
 
 
--- DXReminder/Classes/Tests.cs
+++ DXReminder/Classes/Tests.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using NUnit.Framework;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Collections.ObjectModel;
 
 namespace DXReminder.Classes {
     [TestFixture]
@@ -12,7 +14,7 @@ namespace DXReminder.Classes {
         public void ReminderStoreConstructor() {
             //arrange
             //act
-            var r = new ReminderStore("testdescrption", 4, new DateTime(1, 1, 1, 12, 25, 0));
+            var r = new Reminder("testdescrption", 4, new DateTime(1, 1, 1, 12, 25, 0));
             //assert
             Assert.AreEqual("testdescrption", r.Description);
             Assert.AreEqual(4, r.DayOfWeek);
@@ -30,7 +32,6 @@ namespace DXReminder.Classes {
             //act
             vm.AddNewReminderCommand.Execute(null);
             //assert
-
             Assert.AreEqual(1, vm.Reminders.Count);
             Assert.AreEqual("testd", vm.Reminders[0].Description);
             Assert.AreEqual(6, vm.Reminders[0].DayOfWeek);
@@ -40,11 +41,77 @@ namespace DXReminder.Classes {
         [Test]
         public void ReminderStore_ToString() {
             //arrange
-            ReminderStore r = new ReminderStore("test", 1, new DateTime(1, 1, 1, 15, 15, 0));
+            Reminder r = new Reminder("test", 1, new DateTime(1, 1, 1, 15, 15, 0));
             //act
             string st = r.ToString();
             //assert
             Assert.AreEqual("test - Monday - 15:15", st);
+        }
+        [Test]
+        public void BaseViewModel_CreateSerializerIfNeeded_Yes() {
+            //arrange
+            BaseViewModel vm = new BaseViewModel();
+
+            //act
+            vm.Test_CreateSerializerIfNeeded();
+            var ser1 = vm.Test_Serializer;
+            vm.Test_CreateSerializerIfNeeded();
+            var ser2 = vm.Test_Serializer;
+            //assert
+            Assert.AreEqual(ser1, ser2);
+        }
+        [Test]
+        public void BaseViewModel_CreateSerializerIfNeeded_No() {
+            //arrange
+            BaseViewModel vm = new BaseViewModel();
+
+            //act
+            vm.Test_CreateSerializerIfNeeded();
+            var ser1 = vm.Test_Serializer;
+          
+            //assert
+            Assert.AreNotEqual(null, ser1);
+        }
+        [Test]
+        public void Reminder_GetXML() {
+            //arrange
+            Reminder r = new Reminder("test", 3, new DateTime(1,1,1,14,15,0));
+            XElement xlB = new XElement("Reminder");
+            xlB.Add(new XAttribute("Description", "test"));
+            xlB.Add(new XAttribute("Time", "14:15"));
+            xlB.Add(new XAttribute("DayOfWeek", "3"));
+            //act
+            XElement xl = r.GetXML();
+            var b = XElement.DeepEquals(xl, xlB);
+
+            //assert
+            Assert.AreEqual(true, b);
+        }
+        [Test]
+        public void ReminderSerializer_GetXMLFromReminders() {
+            //arrange
+            Reminder r1 = new Reminder("rem1", 1, new DateTime(1, 1, 1, 11, 11, 0));
+            Reminder r2 = new Reminder("rem2", 2, new DateTime(1, 1, 1, 9, 9, 0));
+            ObservableCollection<Reminder> col = new ObservableCollection<Reminder>();
+            col.Add(r1);
+            col.Add(r2);
+
+            XElement xlBase = new XElement("Reminders");
+            XElement xl1 = new XElement("Reminder");
+            xl1.Add(new XAttribute("Description", "rem1"));
+            xl1.Add(new XAttribute("Time", "11:11"));
+            xl1.Add(new XAttribute("DayOfWeek", "1"));
+            XElement xl2 = new XElement("Reminder");
+            xl2.Add(new XAttribute("Description", "rem2"));
+            xl2.Add(new XAttribute("Time", "09:09"));
+            xl2.Add(new XAttribute("DayOfWeek", "2"));
+            xlBase.Add(xl1);
+            xlBase.Add(xl2);
+
+            //act
+            ReminderSerializer r = new ReminderSerializer();
+            XElement xRes = r.Test_GetXMLFromReminders(col);
+            Boolean b = XElement.DeepEquals(xlBase, xRes);
         }
 
     }
